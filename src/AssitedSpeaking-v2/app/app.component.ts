17ceb7041fdf028068e2c293d6aead53a0b82fd7
OliverJACooke <ocooke@hotmail.com>
2017-02-25 11:54:01 +0000

    first commit and setup

---
+++ src/AssitedSpeaking-v2/app/app.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'as-app',
+    template: `<h1>Hello {{name}}</h1>`
+})
+export class AppComponent {
+    public name: string = 'Angular';
+}
